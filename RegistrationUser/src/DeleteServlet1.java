e889730e9200316397543adea31a804abc2da586
Admin-HR <Admin-HR@10.10.11.167>
2019-09-10 12:26:27 +0530

    please check

--- RegistrationUser/src/DeleteServlet1.java
+++ RegistrationUser/src/DeleteServlet1.java
@@ -10,9 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteServlet
- */
 public class DeleteServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
